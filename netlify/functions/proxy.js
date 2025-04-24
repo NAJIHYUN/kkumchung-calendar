ca67846453377942c8d7d1c6d9bfaabea4eac854
NAJIHYUN <emilyna1004@gmail.com>
2025-04-24 19:12:40 +0900

    Create proxy.js

---
+++ netlify/functions/proxy.js
@@ -0,0 +1,22 @@
+export async function handler(event, context) {
+  const url = "https://calendar.google.com/calendar/ical/kkumchung1@gmail.com/public/basic.ics";
+
+  try {
+    const res = await fetch(url);
+    const text = await res.text();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "text/plain",
+      },
+      body: text,
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: "Fetch error: " + err.message,
+    };
+  }
+}
